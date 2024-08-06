187d20ad9cc55236e187f0604e303545396e9bee
nartkosova <nartkosova21@gmail.com>
2024-08-06 16:30:09 +0300

    exercise 4.9

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -19,7 +19,7 @@ beforeEach(async () => {
     },
     {
       title: 'Blog 2',
-      author: 'Author 2',
+      author: 'Author KA',
       url: 'http://example.com/2',
       likes: 20,
     },
@@ -38,6 +38,17 @@ test('blogs are returned as JSON and correct amount of blogs is returned', async
 
   assert.strictEqual(response.body.length, 2)
 })
+
+test('blog posts have an id property, not _id', async () => {
+    const response = await api.get('/api/blogs')
+  
+    const blogs = response.body
+    blogs.forEach(blog => {
+      assert.ok(blog.id)           
+      assert.strictEqual(blog._id, undefined) 
+      console.log('Blog Posts:', JSON.stringify(blogs, null, 2))
+    })
+  })
 
 after(async () => {
   await mongoose.connection.close() 
