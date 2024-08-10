77994d104c631134baad11ff7510af541a553bd0
nartkosova <nartkosova21@gmail.com>
2024-08-10 20:13:48 +0300

    exercise 4.15

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -4,6 +4,11 @@ const app = require('../app')
 const Blog = require('../models/blog') 
 const { test, after, beforeEach } = require('node:test')
 const assert = require('assert')
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+const Note = require('../models/note')
+
+const helper = require('./test_helper')
 
 const api = supertest(app)
 
@@ -156,6 +161,40 @@ test('blog posts have an id property, not _id', async () => {
   
     assert.strictEqual(updatedBlogFromDb.likes, blogToUpdate.likes + 1);
   });
+  describe('when there is initially one user at db', () => {
+    beforeEach(async () => {
+      await User.deleteMany({})
+  
+      const passwordHash = await bcrypt.hash('sekret', 10)
+      const user = new User({ username: 'root', passwordHash })
+  
+      await user.save()
+    })
+  
+    test('creation succeeds with a fresh username', async () => {
+      const usersAtStart = await helper.usersInDb()
+  
+      const newUser = {
+        username: 'nartush',
+        name: 'Nart Kosova',
+        password: '123123',
+      }
+  
+      await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+  
+      const usersAtEnd = await helper.usersInDb()
+      assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
+  
+      const usernames = usersAtEnd.map(u => u.username)
+      assert(usernames.includes(newUser.username))
+    })
+
+})
+  
   
 after(async () => {
   await mongoose.connection.close() 
