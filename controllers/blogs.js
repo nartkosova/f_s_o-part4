fa6ad4cab1da17fc0e2f6acd17ac052e306f0cfa
nartkosova <nartkosova21@gmail.com>
2024-08-12 19:49:03 +0300

    exercise 4.19

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   try {
@@ -29,8 +38,12 @@ blogsRouter.get('/:id', async (request, response, next) => {
 blogsRouter.post('/', async (request, response) => {
   try {
     const body = request.body;
-    const user = await User.findById(body.userId)
-    
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+        }
+    const user = await User.findById(decodedToken.id)
+       
     if (!body.title || !body.url) {
       return response.status(400).json({ error: 'Title or URL missing' });
     }
