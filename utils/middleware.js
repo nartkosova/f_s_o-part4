ab485be626d354e702537f361fd0f0eef8974788
nartkosova <nartkosova21@gmail.com>
2024-08-12 20:19:41 +0300

    exercise 4.20

--- utils/middleware.js
+++ utils/middleware.js
@@ -27,9 +27,19 @@ const errorHandler = (error, request, response, next) => {
 
   next(error)
 }
-
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('')) {
+    request.token = authorization.substring(7);
+  } else {
+    request.token = null;
+  }
+  console.log('Extracted token:', request.token); 
+  next();
+};
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor
 }
